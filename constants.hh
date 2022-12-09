9c0ff5d5e1b80d49aeae0f01e5b5652f27bc1cc1
ronitchitre <chitreronit@gmial.com>
2022-12-10 01:32:14 +0530

    initialize cpp branch

---
+++ constants.hh
@@ -0,0 +1,9 @@
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+
+namespace car {
+    float max_turn_angle = 10 * (M_PI / 180);
+    float car_length = 1;
+    float car_velocity = 1;
+}
--- constants.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-pi = np.pi
-
-max_turn_angle = 10 * (pi / 180)
-car_length = 1
-car_velocity = 1
