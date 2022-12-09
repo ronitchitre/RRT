1fa1936f3b527e04aa44dbfe562be95d727717ec
ronitchitre <chitreronit@gmial.com>
2022-12-10 00:09:55 +0530

    fix a bug in propogate

--- car.py
+++ car.py
@@ -7,11 +7,14 @@ def rk4method_for_car(ic_car, times):
     h = (times[-1] - times[0]) / N
     w_i = np.concatenate([ic_car.r, ic_car.v])
     for i in range(1, N):
-        t_i = times[0] + h * (i - 1)
-        k1 = h * func(t_i, w_i)
-        k2 = h * func(t_i + h / 2, w_i + k1 / 2)
-        k3 = h * func(t_i + h / 2, w_i + k2 / 2)
-        k4 = h * func(t_i + h, w_i + k3)
+        # t_i = times[0] + h * (i - 1)
+        k1 = h * Car(w_i[0:3], w_i[3:6]).dynamics()
+        w_i_int = w_i + k1 / 2
+        k2 = h * Car(w_i_int[0:3], w_i_int[3:6]).dynamics()
+        w_i_int = w_i + k2 / 2
+        k3 = h * Car(w_i_int[0:3], w_i_int[3:6]).dynamics()
+        w_i_int = w_i + k3
+        k4 = h * Car(w_i_int[0:3], w_i_int[3:6]).dynamics()
         w_i = w_i + (k1 + 2 * k2 + 2 * k3 + k4) / 6
     new_car = Car(w_i[0:3], w_i[3:6])
     return new_car
