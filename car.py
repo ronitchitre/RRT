517fb2b46bb1be47f454040a52827cdc81c328df
ronitchitre <chitreronit@gmial.com>
2022-12-12 01:37:00 +0530

    add tree data structure

--- car.py
+++ car.py
@@ -4,22 +4,21 @@ import constants
 
 pi = np.pi
 
-def rk4method_for_car(ic_car, times):
+def rk4method_for_car(ic_car, omega, times):
     N = len(times)
-    h = (times[-1] - times[0]) / N
-    w_i = np.concatenate([ic_car.r, ic_car.v])
+    h = times[1] - times[0]
+    w_i = np.concatenate([ic_car.x, ic_car.v])
     for i in range(1, N):
         # t_i = times[0] + h * (i - 1)
-        k1 = h * Car(w_i[0:3], w_i[3:6]).dynamics()
+        k1 = h * Car(w_i[0:3], w_i[3:6], 0).dynamics(omega)
         w_i_int = w_i + k1 / 2
-        k2 = h * Car(w_i_int[0:3], w_i_int[3:6]).dynamics()
+        k2 = h * Car(w_i_int[0:3], w_i_int[3:6], 0).dynamics(omega)
         w_i_int = w_i + k2 / 2
-        k3 = h * Car(w_i_int[0:3], w_i_int[3:6]).dynamics()
+        k3 = h * Car(w_i_int[0:3], w_i_int[3:6], 0).dynamics(omega)
         w_i_int = w_i + k3
-        k4 = h * Car(w_i_int[0:3], w_i_int[3:6]).dynamics()
+        k4 = h * Car(w_i_int[0:3], w_i_int[3:6], 0).dynamics(omega)
         w_i = w_i + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-    new_car = Car(w_i[0:3], w_i[3:6])
-    return new_car
+    return w_i[0:3], w_i[3:6]
 
 def normalize(v):
     if np.linalg.norm(v) != 0:
@@ -33,32 +32,31 @@ def ang_between(a, b):
     return np.arcsin(cross / (norm_a * norm_b))
 
 class Car:
-    def __init__(self, x, v, theta, ang_vel = 0):
+    def __init__(self, x, v, theta):
         self.x = x
         self.v = v
         self.theta = theta
-        self.ang_vel = ang_vel
 
-    def dynamics(self):
+    def dynamics(self, omega):
         x_dot = self.v
-        v_dot = np.cross(self.ang_vel, self.v)
-        theta_dot = self.ang_vel
-        return np.array([x_dot, v_dot, theta_dot])
+        v_dot = np.cross(omega, self.v)
+        return np.array([x_dot, v_dot])
 
     def propogate(self, des_node):
         des_direction = des_node - self.x
-        theta = ang_between(des_direction, self.v)
+        alpha = ang_between(des_direction, self.v)
         dist_between_nodes = np.linalg.norm(des_direction)
-        omega = 2 * constants.car_velocity * np.sin(theta) / dist_between_nodes
+        omega = 2 * constants.car_velocity * np.sin(alpha) / dist_between_nodes
         if abs(omega) > constants.max_turn_rate:
             return "des_node invalid"
-        self.ang_vel = omega
         # distance = 2 * theta * (constants.car_velocity / omega)
-        time_taken = 2 * theta / omega
+        time_taken = 2 * alpha / omega
         distance = time_taken * constants.car_velocity
         times = np.linspace(0, time_taken, constants.N_for_rk4)
-        new_car = rk4method_for_car(self, times)
-        return Car()
+        new_x, new_v = rk4method_for_car(self, omega, times)
+        new_theta = self.theta + self.ang_vel * time_taken
+        new_car = Car(new_x, new_v, new_theta)
+        return new_car, distance
         
         
 
--- tree.py
+++ tree.py
@@ -2,6 +2,20 @@ import numpy as np
 import car
 
 class Node(car.Car):
-    def __init__(self, r, v, ster_ang, child):
-        super().__init__(r, v, ster_ang)
-        self.child = child
+    def __init__(self, x, v, theta, parent=None, cost=0):
+        super().__init__(x, v, theta)
+        self.parent = parent
+        self.cost = cost
+        self.child_list = []
+    
+    def add_child(self, child_node):
+        try:
+            new_car, distance = self.propogate(child_node)
+        except:
+            return "child node can not be attached"
+        cost_child = self.cost + distance
+        child_node = Node(new_car.x, new_car.v, new_car.theta, parent=self, cost=cost_child) #might replace new_car.x with child_node.x
+        self.child_list.append(child_node)
+        return child_node 
+
+    
