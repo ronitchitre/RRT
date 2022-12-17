86383e9cf6078ad26242f7d6536e277b035f7e92
ronitchitre <chitreronit@gmial.com>
2022-12-17 14:14:53 +0530

    add random number function

---
+++ random_number_gen.hh
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+float randomFloat()
+{
+	return (float)(rand()) / (float)(RAND_MAX);
+}
+
+int randomInt(int a, int b)
+{
+	if (a > b)
+		return randomInt(b, a);
+	if (a == b)
+		return a;
+	return a + (rand() % (b - a));
+}
+
+float randomFloat(int a, int b)
+{
+	if (a > b)
+		return randomFloat(b, a);
+	if (a == b)
+		return a;
+
+	return (float)randomInt(a, b) + randomFloat();
+}
